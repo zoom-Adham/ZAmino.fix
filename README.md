c862fbc60415669dc77f5ee73c69161a3d85fd8e
zoom-Adham <zoom36976@gmail.com>
2024-05-23 10:08:06 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# ZAmino.fix
+### Telegram
+`https://t.me/ZAminoZ`
+### About lib
+`Fix amino.fix`
+### How to install?
+`pip install ZAmino.fix`
+### API Reference
+[Read the Docs Link](https://aminopy.readthedocs.io/en/latest/)
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    "httpx",
+    "websocket-client==1.3.1", 
+    "setuptools", 
+    "json_minify", 
+    "six",
+    "aiohttp",
+    "websockets"
+]
+
+with open("README.md", "r") as stream:
+    long_description = stream.read()
+
+setup(
+    name="ZAmino.fix",
+    author="ZOOM",
+    version="1.1.7",
+    description="Aminofix update. https://t.me/ZAminoZ",
+    packages=find_packages(),
+    long_description=long_description,
+    install_requires=requirements,
+    keywords=[
+    	'ZAmino'
+    	'ZAmino.fix'
+        'aminoapps',
+        'ZAminofix',
+        'amino',
+        'amino-bot',
+    ],
+    python_requires='>=3.6',
+)
