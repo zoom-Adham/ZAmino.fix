ab29545758024a4bcb71763d0589e60dfb5c51b6
AlternativeOne || Adham <168600906+Alternative0ne@users.noreply.github.com>
2024-05-23 13:02:56 +0300

    Update __init__.py
    Added Version Checker 
    Added Automatic Install lastest Version

--- ZAminofix/__init__.py
+++ ZAminofix/__init__.py
@@ -14,4 +14,24 @@ from .asyncfix import acm, client, sub_client, socket
 from .socket import Callbacks, SocketHandler
 from requests import get
 from json import loads
+from os import system,name
+from colorama import Fore
+# I will cry :(
+import httpx
+try:
+  response = httpx.get("https://pypi.org/pypi/zamino-fix/json")
+  response.raise_for_status()  
+  newest_version = response.json()["info"]["version"]
+except Exception as err:
+  print(f"An error occurred: {err}")
+  newest_version = None
+
+if newest_version and __version__ < newest_version:
+  print(Fore.YELLOW + "[!] You are using old version of zamino.fix, We will update the module automatic for you")
+  system("pip install -U ZAmino.fix")
+  if name == "nt": system("cls") # This for non-unix systems ( windows , etc )
+  else: system("clear") # This For Unix | Unix Like Don't Edit it please
+  print(Fore.YELLOW + f"[!] ZAmino Version : {__version__} - Made By : ZOOM + Adham")
+else:
+  print(Fore.YELLOW + f"[!] ZAmino Version : {__version__} - Made By : ZOOM + Adham") 
 
